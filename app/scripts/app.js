197fcdace44e2e0ba200af9ba8e6dba24d936b73
Wil Burke <billyburke13@gmail.com>
2016-10-23 21:21:59 -0400

    states added

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,27 @@
-anguar.module('blocJams', []);
+(function() {
+  function config($locationProvider, $stateProvider, ) {
+    
+    $locationProvider
+      .html5Mode({
+        enabled: true,
+        requireBase: false
+      });
+
+      $stateProvider
+
+        .state('landing', {
+          url: '/',
+          templateUrl: '/templates/landing.html'
+        })
+
+        .state('album', {
+        url: '/album',
+        templateUrl: '/templates.album.html'
+        });
+  }
+  angular
+    .module('blocJams', ['ui.router'])
+    .config(config);
+
+
+})();
